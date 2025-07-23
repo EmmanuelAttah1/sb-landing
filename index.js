3dbf3a550547d0ad8d9e43651d6c6a1b1923df97
Emmanuel <attahzuzu@gmail.com>
2025-07-23 13:15:10 +0100

    add live link

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const menu = document.getElementById('mobile-menu')
 const openBtn = document.getElementById("open-btn")
 const closeBtn = document.getElementById("close-btn")
 
+const appAddress = "https://speedbuild-app.vercel.app/"
+
 const bookDemo = () => {
     window.open('https://calendly.com/kbdemo1/30min', '_blank');
 }
@@ -18,4 +20,12 @@ const closeMenu=()=>{
     closeBtn.style.setProperty("display","none")
     openBtn.style.setProperty("display",'block')
     document.body.style.overflow = "auto";
+}
+
+const openRegisterPage=()=>{
+    window.open(appAddress+'register');
+}
+
+const openLoginPage=()=>{
+    window.open(appAddress+'login');
 }
